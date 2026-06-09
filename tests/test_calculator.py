4c24aa9e74630e8d0d14a9c1d8b40dc78f54dea6
a.assanov <assanov.assan@icloud.com>
2026-06-09 18:34:15 +0500

    Добавил калькулятор

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -10,8 +10,15 @@ from src.main import Calculator
         (5, -1, -5)
     ]
 )
-def test_divide():
-    x = 1
-    y = 2
-    res = 0.5
-    assert Calculator().divide(x, y) == res
+def test_divide(x, y, res):
+ assert Calculator().divide(x, y) == res
+
+@pytest.mark.parametrize(
+    "x, y, res",
+    [
+        (1, 2, 3),
+        (5, -1, 4)
+    ]
+)
+def test_add(x, y, res):
+ assert Calculator().add(x, y) == res
