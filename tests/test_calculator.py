2a62bfd9dc168f2b56a0f8afd82ea9528a5122ac
a.assanov <assanov.assan@icloud.com>
2026-06-09 19:08:53 +0500

    Добавил калькулятор

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,19 +1,21 @@
 import pytest
+from contextlib import nullcontext as does_not_raise
+
 from src.main import Calculator
 
 class TestCalculator:
 
     @pytest.mark.parametrize(
-        "x, y, res",
+        "x, y, res, expectation",
         [
-            (1, 2, 0.5),
-            (5, -1, -5),
-            (5, "-1", -5)
+            (1, 2, 0.5, does_not_raise()),
+            (5, -1, -5, does_not_raise()),
+            (5, "-1", -5, pytest.raises(TypeError))
 
         ]
     )
-    def test_divide(self, x, y, res):
-        with pytest.raises(TypeError):
+    def test_divide(self, x, y, res, expectation):
+        with expectation:
          assert Calculator().divide(x, y) == res
 
     @pytest.mark.parametrize(
