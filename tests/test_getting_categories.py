40c473a5ffd8a65da71c1dbe692dc51ea57ac6b7
a.assanov <assanov.assan@icloud.com>
2026-06-11 00:21:05 +0500

    Добавляю улучшения для теста categories

--- tests/test_getting_categories.py
+++ tests/test_getting_categories.py
@@ -6,6 +6,7 @@ from src.enums.global_enums import GlobalErrorMessages, GlobalCorrectMessages
 def test_getting_categories():
     response = requests.get(url = SERVICE_URL + '/web/main?apikey=KNnpXL0TCyMm9F0qFqF5vsBzj3O4NGBs&for_kids=false&locale=ru&content_lang=ru&limit=4')
     reseived_banners = response.json()
+    print(reseived_banners)
 
 
     assert response.status_code == 200, GlobalCorrectMessages.CORRECT_STATUS_CODE.value
