8327af32615a2598cc5da87d7ade1001b23ee82b
a.assanov <assanov.assan@icloud.com>
2026-06-11 00:02:28 +0500

    Добавил тест для categories

--- src/enums/global_enums.py
+++ src/enums/global_enums.py
@@ -3,7 +3,9 @@ from enum import Enum
 
 class GlobalCorrectMessages(Enum):
      CORRECT_STATUS_CODE = "Received status code is equal to expected"
+     CORRECT_ELEMENT_COUNT = "Number of items is equal to expected."
 
 class GlobalErrorMessages(Enum):
     WRONG_STATUS_CODE = "Received status code is not equal to expected."
+    WRONG_ELEMENT_COUNT = "Number of items is not equal to expected."
 
--- tests/test_getting_categories.py
+++ tests/test_getting_categories.py
@@ -5,6 +5,10 @@ from src.enums.global_enums import GlobalErrorMessages, GlobalCorrectMessages
 
 def test_getting_categories():
     response = requests.get(url = SERVICE_URL + '/web/main?apikey=KNnpXL0TCyMm9F0qFqF5vsBzj3O4NGBs&for_kids=false&locale=ru&content_lang=ru&limit=4')
+    reseived_banners = response.json()
+
+
     assert response.status_code == 200, GlobalCorrectMessages.CORRECT_STATUS_CODE.value
-    print(response.json())
+    assert len(reseived_banners) == 4, GlobalCorrectMessages.CORRECT_ELEMENT_COUNT.value
+
 
