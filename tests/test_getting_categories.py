db599b398ac21656601eb8af5f6ec03b65ca1006
a.assanov <assanov.assan@icloud.com>
2026-06-11 01:01:37 +0500

    Временно удалил название всех обьектов которые лежат в массиве и оставил только отдельные параметры

--- tests/test_getting_categories.py
+++ tests/test_getting_categories.py
@@ -10,7 +10,7 @@ from src.schemas.get_v2_categories import GET_V2_CATEGORIES
 def test_getting_categories():
     response = requests.get(url = SERVICE_URL + '/web/main?apikey=KNnpXL0TCyMm9F0qFqF5vsBzj3O4NGBs&for_kids=false&locale=ru&content_lang=ru&limit=4')
     reseived_banners = response.json()
-    print(reseived_banners)
+ #   print(reseived_banners)
     assert response.status_code == 200, GlobalCorrectMessages.CORRECT_STATUS_CODE.value
     assert len(reseived_banners) == 4, GlobalCorrectMessages.CORRECT_ELEMENT_COUNT.value
     for banner in reseived_banners:
