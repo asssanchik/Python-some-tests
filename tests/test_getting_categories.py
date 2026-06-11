410746b60dd9ac66c17d08644d3502dcd84dd60f
a.assanov <assanov.assan@icloud.com>
2026-06-11 16:44:27 +0500

    fix

--- tests/test_getting_categories.py
+++ tests/test_getting_categories.py
@@ -4,8 +4,8 @@ from configuration import SERVICE_URL
 
 from src.baseclasses.response import Response
 #from src.schemas.get_v2_categories import GET_V2_CATEGORIES
-
 from src.pydantic_schemas.pydantic_get_v2_categories import PydanticGetV2Categories
+
 def test_getting_categories():
     r = requests.get(url = SERVICE_URL + '/web/main?apikey=KNnpXL0TCyMm9F0qFqF5vsBzj3O4NGBs&for_kids=false&locale=ru&content_lang=ru&limit=4')
     response = Response(r)
