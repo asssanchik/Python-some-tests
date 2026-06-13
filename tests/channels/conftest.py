d3de7edaa3993f05bd275c812f34c5cf5911d7d3
a.assanov <assanov.assan@icloud.com>
2026-06-13 18:59:14 +0500

    Начинаю писать новый тест для channels

--- tests/channels/conftest.py
+++ tests/channels/conftest.py
@@ -1,8 +1,14 @@
 import pytest
 import requests
+from random import randrange
 from configuration import CHANNELS_URL
 
 @pytest.fixture
 def get_api_v1_channels():
     response = requests.get(CHANNELS_URL + '/api/v1/channels?platform=Unico&prev_program=false&device_type=web')
     return response
+
+@pytest.fixture
+def make_number():
+    number = randrange(1, 100, 5)
+    yield 
--- tests/channels/test_get_api_v1_channels.py
+++ tests/channels/test_get_api_v1_channels.py
@@ -6,3 +6,7 @@ from src.baseclasses.response import Response
 def test_getting_api_v1_channels(get_api_v1_channels):
     Response(get_api_v1_channels).assert_status_code(200)
 
+
+@pytest.mark.skip(reason="Not Implemented")
+def test_another():
+    assert 1 == 1
